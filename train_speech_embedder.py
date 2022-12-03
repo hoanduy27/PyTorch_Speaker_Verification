e47c47e5a67a68e3b8f6b9a4e0e7e07c82e81693
duckhoan.hcmut <duckhoan.ds@gmail.com>
2022-12-03 15:05:59 +0700

    add inference function and generate test audio

--- train_speech_embedder.py
+++ train_speech_embedder.py
@@ -11,6 +11,7 @@ import random
 import time
 import torch
 from torch.utils.data import DataLoader
+import torch.nn.functional as F
 
 import hparam
 from hparam import hparam as hp
@@ -112,15 +113,16 @@ def test(model_path):
             enrollment_batch = torch.reshape(enrollment_batch, (hp.test.N*hp.test.M//2, enrollment_batch.size(2), enrollment_batch.size(3)))
             verification_batch = torch.reshape(verification_batch, (hp.test.N*hp.test.M//2, verification_batch.size(2), verification_batch.size(3)))
             
-            perm = random.sample(range(0,verification_batch.size(0)), verification_batch.size(0))
-            unperm = list(perm)
-            for i,j in enumerate(perm):
-                unperm[j] = i
+            # perm = random.sample(range(0,verification_batch.size(0)), verification_batch.size(0))
+            # unperm = list(perm)
+            # for i,j in enumerate(perm):
+            #     unperm[j] = i
+
                 
-            verification_batch = verification_batch[perm]
+            # verification_batch = verification_batch[perm]
             enrollment_embeddings = embedder_net(enrollment_batch)
             verification_embeddings = embedder_net(verification_batch)
-            verification_embeddings = verification_embeddings[unperm]
+            # verification_embeddings = verification_embeddings[unperm]
             
             enrollment_embeddings = torch.reshape(enrollment_embeddings, (hp.test.N, hp.test.M//2, enrollment_embeddings.size(1)))
             verification_embeddings = torch.reshape(verification_embeddings, (hp.test.N, hp.test.M//2, verification_embeddings.size(1)))
@@ -160,67 +162,41 @@ def inference(model_path, hp):
     enroll_dl = DataLoader(enroll_set, batch_size=hp.train.N, shuffle=True, num_workers=hp.test.num_workers, drop_last=True)
     hp.training= False 
     predict_set = SpeakerDatasetTIMIT(hp)
-    predict_dl = DataLoader(enroll_set, batch_size=hp.test.N, shuffle=True, num_workers=hp.test.num_workers, drop_last=True)
+    predict_dl = DataLoader(predict_set, batch_size=hp.test.N, shuffle=True, num_workers=hp.test.num_workers, drop_last=True)
+    verification_batch = next(iter(predict_dl))
 
-    
     embedder_net = SpeechEmbedder()
     embedder_net.load_state_dict(torch.load(model_path))
     embedder_net.eval()
     
-    avg_EER = 0
-    for e in range(hp.test.epochs):
-        batch_avg_EER = 0
-        for batch_id, mel_db_batch in enumerate(test_loader):
-            assert hp.test.M % 2 == 0
-            enrollment_batch, verification_batch = torch.split(mel_db_batch, int(mel_db_batch.size(1)/2), dim=1)
-            
-            enrollment_batch = torch.reshape(enrollment_batch, (hp.test.N*hp.test.M//2, enrollment_batch.size(2), enrollment_batch.size(3)))
-            verification_batch = torch.reshape(verification_batch, (hp.test.N*hp.test.M//2, verification_batch.size(2), verification_batch.size(3)))
-            
-            perm = random.sample(range(0,verification_batch.size(0)), verification_batch.size(0))
-            unperm = list(perm)
-            for i,j in enumerate(perm):
-                unperm[j] = i
-                
-            verification_batch = verification_batch[perm]
-            enrollment_embeddings = embedder_net(enrollment_batch)
-            verification_embeddings = embedder_net(verification_batch)
-            verification_embeddings = verification_embeddings[unperm]
-            
-            enrollment_embeddings = torch.reshape(enrollment_embeddings, (hp.test.N, hp.test.M//2, enrollment_embeddings.size(1)))
-            verification_embeddings = torch.reshape(verification_embeddings, (hp.test.N, hp.test.M//2, verification_embeddings.size(1)))
-            
-            enrollment_centroids = get_centroids(enrollment_embeddings)
-            
-            sim_matrix = get_cossim(verification_embeddings, enrollment_centroids)
-            
-            # calculating EER
-            diff = 1; EER=0; EER_thresh = 0; EER_FAR=0; EER_FRR=0
-            
-            for thres in [0.01*i+0.5 for i in range(50)]:
-                sim_matrix_thresh = sim_matrix>thres
-                
-                FAR = (sum([sim_matrix_thresh[i].float().sum()-sim_matrix_thresh[i,:,i].float().sum() for i in range(int(hp.test.N))])
-                /(hp.test.N-1.0)/(float(hp.test.M/2))/hp.test.N)
+    enrollment_batch = next(iter(enroll_dl))
+
+    enrollment_batch = torch.squeeze(enrollment_batch, 0)
+    verification_batch = torch.squeeze(verification_batch, 0)
     
-                FRR = (sum([hp.test.M/2-sim_matrix_thresh[i,:,i].float().sum() for i in range(int(hp.test.N))])
-                /(float(hp.test.M/2))/hp.test.N)
-                
-                # Save threshold when FAR = FRR (=EER)
-                if diff> abs(FAR-FRR):
-                    diff = abs(FAR-FRR)
-                    EER = (FAR+FRR)/2
-                    EER_thresh = thres
-                    EER_FAR = FAR
-                    EER_FRR = FRR
-            batch_avg_EER += EER
-            print("\nEER : %0.2f (thres:%0.2f, FAR:%0.2f, FRR:%0.2f)"%(EER,EER_thresh,EER_FAR,EER_FRR))
-        avg_EER += batch_avg_EER/(batch_id+1)
-    avg_EER = avg_EER / hp.test.epochs
-    print("\n EER across {0} epochs: {1:.4f}".format(hp.test.epochs, avg_EER))
+    enrollment_embeddings = embedder_net(enrollment_batch)
+    verification_embeddings = embedder_net(verification_batch)
+
+    enrollment_embeddings = torch.unsqueeze(enrollment_embeddings, 0)
+    verification_embeddings = torch.unsqueeze(verification_embeddings, 0)
+
+    enrollment_centroids = get_centroids(enrollment_embeddings)
+
+    embedding_flat = torch.flatten(verification_embeddings)
+    centroid_flat = torch.flatten(enrollment_centroids)
+
+    # print(embedding_flat.shape)
+    # print(centroid_flat.shape)
+    # raise
+
+    distance = F.cosine_similarity(embedding_flat, centroid_flat, 0)
+    print(distance)
+
         
 if __name__=="__main__":
-    if hp.training:
-        train(hp.model.model_path)
-    else:
-        test(hp.model.model_path)
+    inference(hp.model.model_path, hp)
+    # test(hp.model.model_path)
+    # if hp.training:
+    #     train(hp.model.model_path)
+    # else:
+    #     test(hp.model.model_path)
